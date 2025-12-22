e16f36030c8f8a858d3d1d6c879fb168fd0fce3c
Jiaxin Zhang <37864938+jacazjx@users.noreply.github.com>
2025-12-22 17:03:36 +0100

    Add files via upload

---
+++ run_client.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import torch
+import argparse
+from client import ClientCluster
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_dir', type=str, default="../../Datasets")
+    parser.add_argument('--device', choices=['cuda', 'cpu'], help="use cuda or cpu")
+    parser.add_argument('-g', '--gpu', type=int, default="0", help="gpu id")
+    parser.add_argument('--port', type=int, default=12345)
+    parser.add_argument('--timeout', type=int, default=7200)
+    parser.add_argument('--buffer_size', type=int, default=1048576, help="initial buffer size")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    if args.device is None:
+        args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+        args.device = torch.device(args.device)
+    if args.device == torch.device("cuda"):
+        torch.cuda.set_device(args.gpu)
+
+    if not os.path.exists(args.data_dir):
+        os.mkdir(args.data_dir)
+        raise FileExistsError(f'Not Found {args.data_dir}')
+
+    print(args)
+    print(f'#### Run Client ####')
+    client_cluster = ClientCluster(args.port)
+    client_cluster.run(args)
